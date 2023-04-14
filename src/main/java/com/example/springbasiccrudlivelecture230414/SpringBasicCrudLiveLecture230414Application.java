4efbf7d0e8ea5790bcd1f4eb40c95aa07300f321
lecture <Robbie>
2023-04-14 15:00:19 +0900

    inti commit

---
+++ src/main/java/com/example/springbasiccrudlivelecture230414/SpringBasicCrudLiveLecture230414Application.java
@@ -0,0 +1,13 @@
+package com.example.springbasiccrudlivelecture230414;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicCrudLiveLecture230414Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicCrudLiveLecture230414Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasiccrudlivelecture230414/SpringBasicCrudLiveLecture230414ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasiccrudlivelecture230414;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicCrudLiveLecture230414ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
