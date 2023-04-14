8a06e48a62fc2ad644df661998f95041659784a1
lecture <Robbie>
2023-04-14 21:27:13 +0900

    Timestamped 적용

--- src/main/java/com/example/springbasiccrudlivelecture230414/SpringBasicCrudLiveLecture230414Application.java
+++ src/main/java/com/example/springbasiccrudlivelecture230414/SpringBasicCrudLiveLecture230414Application.java
@@ -2,7 +2,9 @@ package com.example.springbasiccrudlivelecture230414;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBasicCrudLiveLecture230414Application {
 
